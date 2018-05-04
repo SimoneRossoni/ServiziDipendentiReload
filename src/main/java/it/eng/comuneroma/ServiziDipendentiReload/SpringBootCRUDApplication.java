7823078a0a9f171ac39183418033981d00b05c36
Simone <Simone@192.168.147.1>
2018-05-04 18:09:47 +0200

    prova context-path

--- src/main/java/it/eng/comuneroma/ServiziDipendentiReload/SpringBootCRUDApplication.java
+++ src/main/java/it/eng/comuneroma/ServiziDipendentiReload/SpringBootCRUDApplication.java
@@ -2,8 +2,12 @@ package it.eng.comuneroma.ServiziDipendentiReload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+import it.eng.comuneroma.ServiziDipendentiReload.configuration.JpaConfiguration;
+
+@Import(JpaConfiguration.class)
+@SpringBootApplication(scanBasePackages={"it.eng.comuneroma.ServiziDipendentiReload"})
 public class SpringBootCRUDApplication {
 	
     public static void main(String[] args) {
